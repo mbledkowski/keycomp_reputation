c4727fe6b374ba610fc4d964e715fda7dcc38d3c
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2021-10-02 17:44:16 +0200

    Implemented twitter and reddit API

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 import psycopg
+import tweepy
+import praw
+import git
 
-from bs4 import BeautifulSoup
-
-import sys
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWebEngineWidgets import QWebEnginePage
-
-class Page(QWebEnginePage):
-    def __init__(self, url):
-        self.app = QApplication(sys.argv)
-        QWebEnginePage.__init__(self)
-        self.html = ''
-        self.loadFinished.connect(self._on_load_finished)
-        self.load(QUrl(url))
-        self.app.exec_()
-
-    def _on_load_finished(self):
-        self.html = self.toHtml(self.Callable)
-        print('Load finished')
-
-    def Callable(self, html_str):
-        self.html = html_str
-        self.app.quit()
+repo = git.Repo(search_parent_directories=True)
 
 cridentials = open(".env", "r").read().splitlines()
+
+twitter = tweepy.Client(bearer_token=cridentials[4])
+
+reddit = praw.Reddit(
+    client_id=cridentials[5],
+    client_secret=cridentials[6],
+    password=cridentials[7],
+    user_agent="keycomp by u/mble",
+    username="mble",
+)
+
+twitterProfiles = []
+redditProfiles = []
+
+authors=[]
 
 with psycopg.connect(
   host=cridentials[0],
@@ -38,11 +34,54 @@ with psycopg.connect(
       FROM authors
       WHERE "isReviewer" = true
     """)
-    for i in cur.fetchall():
-      page = Page(i[0][1][1:-1])
-      print(page.html)
-      soup = BeautifulSoup(page.html, 'html.parser')
+    authors = cur.fetchall()
 
-      followElement = soup.select('div.r-13awgt0:nth-child(5) > div:nth-child(2) > a:nth-child(1) > span:nth-child(1) > span:nth-child(1)')
+    for i in authors:
+      iTwitter = i[0][1]
+      iReddit = i[0][2]
+      if iTwitter != None:
+        twitterProfiles.append(iTwitter[21:-1])
 
-      print(followElement)
+      if iReddit != None:
+        redditProfiles.append(iReddit[29:-1])
+
+    twitterStats=[]
+    redditStats=[]
+
+    for i in twitter.get_users(usernames=twitterProfiles, user_fields=["public_metrics"]).data:
+      try:
+        twitterStats.append(i.public_metrics['followers_count'])
+      except:
+        twitterStats.append(0)
+
+    for i in redditProfiles:
+      try:
+        redditStats.append(reddit.redditor(i).comment_karma)
+      except:
+        redditStats.append(0)
+
+    t =0
+    r = 0
+    for index, i in enumerate(authors):
+      iTwitter = i[0][1]
+      iReddit = i[0][2]
+      xNull = False
+      yNull = False
+      if iTwitter != None:
+        x=twitterStats[t]
+        t += 1
+      else:
+        x=-1
+
+      if iReddit != None:
+        y=redditStats[r]
+        r += 1
+      else:
+        y=-1
+
+      print("""
+      INSERT INTO reputation(authorid,twitterFollowers, redditKarma, score, software)
+      VALUES ({authorid}, {tF}, {rK}, {score}, \"{soft}\");
+      """.format(authorid=i[0][0], tF="NULL" if x==-1 else x, rK="NULL" if y==-1 else y, score=((x/(x+256))+2*(y/(y+2048)))/3, soft = f"mbledkowski/keycomp_reputation v1 {repo.head.object.hexsha}"
+))
+      

abfd1d78c327301656c3afe15bd9a7ce966cc221
Maciej Błędkowski <23295125+mbledkowski@users.noreply.github.com>
2021-10-03 09:13:21 +0200

    Fixed INSERT INTO VALUES element

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ with psycopg.connect(
         y=-1
 
       cur.execute("""
-      INSERT INTO reputation(authorid,twitterFollowers, redditKarma, score, software)
-      VALUES ({authorid}, {tF}, {rK}, {score}, \"{soft}\");
+      INSERT INTO reputation(authorid, "twitterFollowers", "redditKarma", score, software)
+      VALUES ({authorid}, {tF}, {rK}, {score}, '{soft}');
       """.format(authorid=i[0][0], tF="NULL" if x==-1 else x, rK="NULL" if y==-1 else y, score=((x/(x+256))+2*(y/(y+2048)))/3, soft = f"mbledkowski/keycomp_reputation {sorted(repo.tags, key=lambda t: t.commit.committed_datetime)[-1]} {repo.head.object.hexsha[0:7]}"
 ))
